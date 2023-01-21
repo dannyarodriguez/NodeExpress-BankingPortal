4f73c76334252290eef9854972f416aa7045a722
dannyarodriguez <danny.accenture@outlook.com>
2023-01-20 22:35:40 -0600

    Completed module4

--- src/app.js
+++ src/app.js
@@ -1,19 +1,13 @@
 const fs = require('fs')
 const path = require('path')
-const express = require('express');
 
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const accountData = fs.readFileSync("src/json/accounts.json", "UTF8");
-const accounts = JSON.parse(accountData);
+const { accounts, users, writeJSON } = require('./data.js');
 
-const userData = fs.readFileSync("src/json/users.json", "UTF8");
-const users = JSON.parse(userData);
-
-const accountRoutes = require("./routes/accounts");
-
-
+//const accountRoutes = require("./routes/accounts");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -21,7 +15,7 @@ app.set("views", path.join(__dirname, "/views"));
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/account", accountRoutes);
+//app.use("/account", accountRoutes);
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
@@ -35,8 +29,7 @@ app.get('/transfer', (req, res) =>  res.render('transfer'));
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance -= req.body.amount;
     accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 });
 
@@ -44,8 +37,7 @@ app.get('/payment', (req, res) => res.render('payment', {account: accounts.credi
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 

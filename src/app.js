e32c27227880f411f50b88c6025f56d930f87a7e
dannyarodriguez <danny.accenture@outlook.com>
2023-01-20 22:04:54 -0600

    Completed module 2

--- src/app.js
+++ src/app.js
@@ -3,8 +3,17 @@ const path = require('path')
 const express = require('express');
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+const accountData = fs.readFileSync("src/json/accounts.json", "UTF8");
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync("src/json/users.json", "UTF8");
+const users = JSON.parse(userData);
+
+const accountRoutes = require("./routes/accounts");
+
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -14,9 +23,15 @@ app.use(express.static(path.join(__dirname, "./public/css/styles.css")));
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.get("/", (req, res) => {
-    res.render("index", { title: "Account Summary" });
-  });
+app.use("/account", accountRoutes);
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
